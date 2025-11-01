70fa5d7ed7797134ae54059fe72ffbb1ca876244
Anthony-QQ <116741764+Anthony-QQ@users.noreply.github.com>
2025-11-01 23:17:34 +0800

    Commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from plot_parameters import plot_now
 from GEO_satellite_image_plotter import plot_files
 
 
-TC_list = ['19_Dorian']
+TC_list = ['25_Melissa']
 f_num_rel = 0.4 #where to start plotting, from 0 to 1
 plot_count = 1
 
